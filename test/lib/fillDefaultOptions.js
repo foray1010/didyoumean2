7fd42b2b6d3b6e197b4525fd86883998ae1a34b7
foray1010 <foray1010@users.noreply.github.com>
2016-01-04 00:55:21 +0800

    split schema to simpleSchema

--- test/lib/fillDefaultOptions.js
+++ test/lib/fillDefaultOptions.js
@@ -11,42 +11,6 @@ const defaultOptions = {
   threshold: 0.4
 }
 
-test('test without arguments', (t) => {
+test('test: without arguments', (t) => {
   t.same(fillDefaultOptions(), defaultOptions)
-})
-
-test('test empty options', (t) => {
-  t.same(fillDefaultOptions({}), defaultOptions)
-})
-
-test('test options schema', (t) => {
-  t.throws(() => {
-    fillDefaultOptions({
-      caseSensitive: {}
-    })
-  }, TypeError)
-
-  t.throws(() => {
-    fillDefaultOptions({
-      matchPath: {}
-    })
-  }, TypeError)
-
-  t.throws(() => {
-    fillDefaultOptions({
-      returnType: {}
-    })
-  }, TypeError)
-
-  t.throws(() => {
-    fillDefaultOptions({
-      returnType: 'wrong enum'
-    })
-  }, RangeError)
-
-  t.throws(() => {
-    fillDefaultOptions({
-      caseSensitive: {}
-    })
-  }, TypeError)
 })
