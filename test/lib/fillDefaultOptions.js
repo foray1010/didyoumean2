43ce600d6cbf720f4273660dd64d524ee97d226c
foray1010 <foray1010@gmail.com>
2016-01-05 22:33:09 +0800

    Default value of threshold with thresholdType:"edit-distance" is now 20

--- test/lib/fillDefaultOptions.js
+++ test/lib/fillDefaultOptions.js
@@ -1,19 +1,39 @@
 'use strict'
 
+const Immutable = require('seamless-immutable')
 const test = require('ava')
 
 const fillDefaultOptions = require('../../lib/fillDefaultOptions')
 const returnTypeEnums = require('../../enums/returnTypeEnums')
 const thresholdTypeEnums = require('../../enums/thresholdTypeEnums')
 
-const defaultOptions = {
+const FIRST_CLOSEST_MATCH = returnTypeEnums.FIRST_CLOSEST_MATCH
+
+const EDIT_DISTANCE = thresholdTypeEnums.EDIT_DISTANCE
+const SIMILARITY = thresholdTypeEnums.SIMILARITY
+
+const defaultOptions = Immutable({
   caseSensitive: false,
   matchPath: '',
-  returnType: returnTypeEnums.FIRST_CLOSEST_MATCH,
+  returnType: FIRST_CLOSEST_MATCH,
   threshold: 0.4,
-  thresholdType: thresholdTypeEnums.SIMILARITY
-}
+  thresholdType: SIMILARITY
+})
+
+const defaultOptionsForEditDistance = Immutable({
+  caseSensitive: false,
+  matchPath: '',
+  returnType: FIRST_CLOSEST_MATCH,
+  threshold: 20,
+  thresholdType: EDIT_DISTANCE
+})
 
 test('without arguments', (t) => {
   t.same(fillDefaultOptions(), defaultOptions)
+})
+
+test(`thresholdType: "${EDIT_DISTANCE}"`, (t) => {
+  t.same(fillDefaultOptions({
+    thresholdType: EDIT_DISTANCE
+  }), defaultOptionsForEditDistance)
 })
