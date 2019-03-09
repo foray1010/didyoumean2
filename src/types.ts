265f98cf8612cbefe363eee56254cc60dbc63467
Alex Young <foray1010@users.noreply.github.com>
2019-03-09 10:07:04 +0800

    refactor: use Partial type to rewrite InputOptions type

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,6 @@
 import {ReturnTypeEnums} from './enums/ReturnTypeEnums'
 import {ThresholdTypeEnums} from './enums/ThresholdTypeEnums'
 
-export interface InputOptions {
-  caseSensitive?: boolean
-  deburr?: boolean
-  matchPath?: Array<number | string>
-  returnType?: ReturnTypeEnums
-  threshold?: number
-  thresholdType?: ThresholdTypeEnums
-  trimSpaces?: boolean
-}
-
 export interface Options {
   caseSensitive: boolean
   deburr: boolean
@@ -20,3 +10,5 @@ export interface Options {
   thresholdType: ThresholdTypeEnums
   trimSpaces: boolean
 }
+
+export type InputOptions = Partial<Options>
