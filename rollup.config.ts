ab96a8c00fe58af88716a90dec07a693d4c043ea
Alex Young <foray1010@users.noreply.github.com>
2019-06-07 20:38:51 +0800

    feat: provide umd build

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,6 +3,7 @@ import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import dts from 'rollup-plugin-dts'
 import json from 'rollup-plugin-json'
+import resolve from 'rollup-plugin-node-resolve'
 // @ts-ignore
 import {terser} from 'rollup-plugin-terser'
 
@@ -36,6 +37,27 @@ export default [
     watch: {
       clearScreen: false
     }
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      exports: 'named',
+      file: 'dist/index.umd.js',
+      format: 'umd',
+      name: 'didYouMean',
+      sourcemap: true
+    },
+    plugins: [
+      babel({
+        extensions: ['.ts']
+      }),
+      commonjs({
+        extensions: ['.js', '.ts']
+      }),
+      resolve(),
+      json(),
+      terser()
+    ]
   },
   {
     input: 'dist-dts/index.d.ts',
