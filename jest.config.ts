e72345a3266fabbc12b73b3715a12e74ca84208b
Alex Young <foray1010@users.noreply.github.com>
2021-10-09 11:13:20 +0800

    chore: support .cts and .mts

--- jest.config.ts
+++ jest.config.ts
@@ -2,9 +2,9 @@ import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.{cjs,js,mjs,ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{cjs,cts,js,mjs,mts,ts,tsx}'],
   coverageReporters: ['lcov', 'text-summary'],
-  testMatch: ['**/*.{spec,test}.{cjs,js,mjs,ts,tsx}'],
+  testMatch: ['**/*.{spec,test}.{cjs,cts,js,mjs,mts,ts,tsx}'],
 }
 
 export default config
