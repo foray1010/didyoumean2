a08f68691d48801371a1cee78934551a3a9f32a0
Alex Young <foray1010@users.noreply.github.com>
2022-06-11 21:29:40 +0100

    fixup! chore(deps): update jest monorepo

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from '@jest/types'
+import type { Config } from 'jest'
 
-const config: Config.InitialOptions = {
+const config: Config = {
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.{cjs,cts,js,mjs,mts,ts,tsx}'],
   coverageReporters: ['lcov', 'text-summary'],
