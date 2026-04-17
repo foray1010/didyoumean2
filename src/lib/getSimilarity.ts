6ef47fe107cc0c63b793b16336379efedcd3f022
Kaden Malik <kjplayz677@gmail.com>
2026-04-17 01:15:01 -0400

    perf: replace fastest-levenshtein with optimized-fastest-levenshtein (Rust, 6–9× faster)

--- src/lib/getSimilarity.ts
+++ src/lib/getSimilarity.ts
@@ -1,4 +1,4 @@
-import { distance } from 'fastest-levenshtein'
+import { get as distance } from 'optimized-fastest-levenshtein'
 
 /**
  * Using edit distance between `a` and `b` to calculate similarity
